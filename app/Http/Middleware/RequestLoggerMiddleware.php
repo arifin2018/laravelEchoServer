2cc59cb0632278ea2a3c39500c54855a7e0f9de4
arifinlenna <arifin@lenna.ai>
2023-09-03 05:12:23 +0700

    RequestLoggerMiddleware

---
+++ app/Http/Middleware/RequestLoggerMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+
+class RequestLoggerMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $this->Logging($request);
+        return $next($request);
+    }
+
+    private function Logging(Request $request):void {
+        Log::info(json_encode($request));
+    }
+}
